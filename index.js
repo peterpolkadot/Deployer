97b74faa2e7e2896a00b677050f12702fc925215
peterpolkadot <154844362+peterpolkadot@users.noreply.github.com>
2025-11-12 07:41:27 +1000

    Add index.js via Apps Script

---
+++ index.js
@@ -0,0 +1,7 @@
+
+// === Test Deployment File ===
+// Created via Google Apps Script ? GitHub ? Vercel
+
+export default function handler(req, res) {
+  res.status(200).json({ message: "? Hello from Vercel via Apps Script!" });
+}
